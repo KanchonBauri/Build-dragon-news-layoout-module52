a9986dc2b45551803146033c30f0e84a18dc3b24
KanchonBauri <kanchonbauri1998@gmail.com>
2024-11-14 17:40:20 +0600

    main layout  module-52.6

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -11,12 +11,15 @@ const HomeLayout = () => {
                 <section className='w-11/12 mx-auto'>
                     <LatestNews></LatestNews>
                 </section>
-                <section className='w-11/12 mx-auto py-2'>
-                    <Navbar></Navbar>
-                </section>
             </header>
-            <nav></nav>
-            <main></main>
+            <nav className='w-11/12 mx-auto py-2'>
+                <Navbar></Navbar>
+            </nav>
+            <main className='w-11/12 mx-auto pt-5 grid grid-cols-12 gap-3  '>
+                <aside className='left-0 col-span-3 '>Left Navbar</aside>
+                <section className='col-span-6'>Main Section</section>
+                <aside className=' col-span-3 '>Righ Navbar</aside>
+            </main>
         </div>
     );
 };
