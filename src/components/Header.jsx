d11a6783a1a3e4f5be91f679be3b8fadb5a7b6f3
KanchonBauri <kanchonbauri1998@gmail.com>
2024-11-14 17:23:13 +0600

    integrate font, design navigation module-52.6

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,11 @@ import moment from 'moment';
 
 const Header = () => {
     return (
-        <div className='flex flex-col justify-center items-center gap-2 py-2 '>
+        <div className='flex flex-col justify-center items-center gap-2 py-2 font-poppins '>
             <div>
                 <img src={logo} alt="" />
             </div>
-            <h2 className='text-gray-400'>Journalism Without fear Or Favour</h2>
+            <h2 className='text-gray-400 '>Journalism Without fear Or Favour</h2>
              {/* <p>{moment().format("dddd, MMMM Do YYYY, h:mm:ss a")}</p>  Thursday, November 14th 2024, 2:46:01 pm */}
             <p>{moment().format("dddd, MMMM Do YYYY")}</p>
         </div>
